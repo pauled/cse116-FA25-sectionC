00d877223f04d4c5553961acc160f6e44addd302
pauled <drpauled@gmail.com>
2025-09-26 10:56:51 -0400

    day5-3

--- src/week5/TestBank.java
+++ src/week5/TestBank.java
@@ -4,8 +4,19 @@ import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class TestBank {
+    public static boolean testAccounts(BankAccount b1,BankAccount b2){
+        if (!b1.getAccountHolder().equals(b2.getAccountHolder())){
+            return false;
+        }
+        if (! (b1.checkBalance()==b2.checkBalance())){
+            return false;
+        }
+        return true;
+    }
     @Test
     public void testBank(){
         Bank bank=new Bank();
@@ -24,5 +35,15 @@ public class TestBank {
                         new BankAccount("b",2),
                         new BankAccount("d",4)
         ));
+
+        for (BankAccount exp : expected){
+            boolean found=false;
+            for (BankAccount act : actual){
+                if (testAccounts(exp,act)){
+                    found=true;
+                }
+            }
+            assertTrue(found);
+        }
     }
 }
